9682e3e5e875f2012916715b90e8fd38c91b3482
Timofey Goncharov <sedmoy.mm@mail.ru>
2018-11-21 00:25:45 +0300

    Refactor index.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import Home from '@containers/home';
-import Search from '@containers/search';
-import mainStyle from '@styles/main.scss';
+import '@styles/main.scss';
 import createStore from './store/create-store';
-
 
 const history = createBrowserHistory();
 
