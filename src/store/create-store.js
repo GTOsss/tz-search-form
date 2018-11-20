60c702775ce6a069d90ad271747ba3fb8cfdf8ee
Timofey Goncharov <sedmoy.mm@mail.ru>
2018-11-20 17:19:36 +0300

    Fix create-store.

--- src/store/create-store.js
+++ src/store/create-store.js
@@ -1,10 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
+import { reducer as reduxFormReducer } from 'redux-form';
+import projectReducers from './reducers';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const middleware = applyMiddleware(thunk);
-const enhancer = isProduction ? composeWithDevTools(middleware) : middleware;
+const enhancer = !isProduction ? composeWithDevTools(middleware) : middleware;
+const reducers = {
+  ...projectReducers,
+  reduxForm: reduxFormReducer,
+};
 
-export default () => createStore(combineReducers({ ...reducers }), enhancer);
+export default () => createStore(combineReducers(reducers), enhancer);
