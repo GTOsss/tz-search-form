9df7a98dbed03367529d5e79a9a863a69fd11c9d
Timofey Goncharov <sedmoy.mm@mail.ru>
2018-11-21 00:59:41 +0300

    Expand search-form and add styles.

--- src/blocks/search-form/index.js
+++ src/blocks/search-form/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
-import { Checkbox, Button } from '@components/inputs';
+import { Checkbox, Button, InputSelect, InputSliderRange } from '@components/inputs';
+import formStyle from '@styles/blocks/form.scss';
+import style from '@styles/blocks/search-form.scss';
+import cx from 'classnames';
+
+const countRoomsOptions = [
+  { label: 'Студия', value: 'studio' },
+  { label: '1-комнатная', value: '1' },
+  { label: '2-комнтаная', value: '2' },
+  { label: '3-комнатная', value: '3' },
+  { label: '4-комнатная', value: '4' },
+  { label: '5-комнтаная', value: '5' },
+  { label: '6-комнатная', value: '6' },
+];
 
 const SearchForm = ({ handleSubmit }) => (
-  <form onSubmit={handleSubmit}>
-    <Field id="mortgage" name="mortgage" component={Checkbox}>Ипотека</Field>
-    <Field id="installment" name="installment" component={Checkbox}>Рассрочка</Field>
-    <Button type="submit">Подобрать</Button>
+  <form className={cx(formStyle.formDefault, style.searchForm)} onSubmit={handleSubmit}>
+    <div className={style.wrapInputs}>
+      <div className={style.wrapCheckboxes}>
+        <Field id="mortgage" name="mortgage" component={Checkbox}>Ипотека</Field>
+        <Field id="installment" name="installment" component={Checkbox}>Рассрочка</Field>
+      </div>
+      <div className={style.wrapSelectAndPrice}>
+        <Field name="countRooms" component={InputSelect} isMulti options={countRoomsOptions} />
+        <Field name="price" component={InputSliderRange} />
+      </div>
+    </div>
+    <Button className={style.buttonSubmit} type="submit">Подобрать</Button>
   </form>
 );
 
